ed8e3ddeaa1db4bf05dd5da73a8e17234667300c
maninserg <manin.serg@gmail.com>
2020-10-25 13:42:11 +0300

    Addition the one file witn settings of game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,26 +2,28 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     """Initialization game and create screen
 
     """
 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    bg_color = (230, 230, 230) #Assingment color of background
 
+    # Start main cycle of program
     while True:
-        """Look events keyboard and mouse
-
-        """
+        #Look events of keyboard and mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            screen.fill(bg_color) # For every step refresh screen
+            screen.fill(ai_settings.bg_color) # For every step refresh screen
             pygame.display.flip()
 
 run_game()
---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    """ Class for saving all settings of game
+
+    """
+    def __init__(self):
+        """ Initialition of settings of game
+
+        """
+        #Width and height of screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        #Color of screen
+        self.bg_color = (230, 230, 230)
